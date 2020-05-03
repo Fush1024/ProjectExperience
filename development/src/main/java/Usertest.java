f0db13c04d63b3293da9ad9ebc0f4369d5db087a
Corsairmaster <1363244352@qq.com>
2020-05-03 18:51:39 +0800

    example : a maven project

---
+++ development/src/main/java/Usertest.java
@@ -0,0 +1,10 @@
+/**
+ * @Author lidengjin
+ * @Date 2020/5/3 6:46 下午
+ * @Version 1.0
+ */
+public class Usertest {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
