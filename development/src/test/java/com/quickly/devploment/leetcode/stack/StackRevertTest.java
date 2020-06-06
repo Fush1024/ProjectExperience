0f661ed05c34c1c7586d3f890de8f4dc6451f152
lidengjin <13488601486@163.com>
2020-06-06 12:56:14 +0800

    通过栈实现括号匹配问题--hashmap，栈pop push 思想

--- development/src/test/java/com/quickly/devploment/leetcode/stack/StackRevertTest.java
+++ development/src/test/java/com/quickly/devploment/leetcode/stack/StackRevertTest.java
@@ -3,6 +3,10 @@ package com.quickly.devploment.leetcode.stack;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 /**
  * @Author lidengjin
  * @Date 2020/6/5 3:19 下午
@@ -10,6 +14,16 @@ import org.junit.Test;
  */
 @Slf4j
 public class StackRevertTest {
+
+	private static Map<Character, Character> bracketCharHashMap;
+
+	static {
+		bracketCharHashMap = new HashMap<>();
+		bracketCharHashMap.put(')','(');
+		bracketCharHashMap.put('}','{');
+		bracketCharHashMap.put(']','[');
+	}
+
 	/**
 	 * 测试是否是回文数 通过栈的思想
 	 */
@@ -38,6 +52,37 @@ public class StackRevertTest {
 		} else
 			System.out.println("no");
 	}
+
+	/**
+	 * 括号匹配 stack
+	 */
+	@Test
+	public void testBracketStack(){
+		String bracket = "({[]}{}(())()){}";
+		System.out.println(bracketStack(bracket));
+	}
+
+
+	private boolean bracketStack(String bracket) {
+		bracket = bracket.trim();
+		char[] chars = bracket.toCharArray();
+		Stack<Character> stack = new Stack();
+		if(chars.length % 2 != 0 ){
+			return false;
+		}
+		for (int i = 0; i < chars.length; i++) {
+			char aChar = chars[i];
+			if(bracketCharHashMap.containsKey(aChar)){
+				return bracketCharHashMap.get(aChar) == stack.pop();
+			}
+			stack.push(aChar);
+		}
+		if(stack.isEmpty()){
+			return true;
+		}
+		return false;
+	}
+
 }
 
 
