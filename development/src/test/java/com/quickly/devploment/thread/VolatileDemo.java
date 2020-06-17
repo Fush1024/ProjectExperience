ed621a1fc818349bc62c4f882db006f2480fac13
lidengjin <13488601486@163.com>
2020-06-17 13:46:53 +0800

    Volatile JMM 静态变量是否刷新主存，和编译器的编译有关。

--- development/src/test/java/com/quickly/devploment/thread/VolatileDemo.java
+++ development/src/test/java/com/quickly/devploment/thread/VolatileDemo.java
@@ -1,6 +1,7 @@
 package com.quickly.devploment.thread;
 
 import lombok.SneakyThrows;
+import org.junit.Test;
 
 /**
  * @Author lidengjin
@@ -8,6 +9,9 @@ import lombok.SneakyThrows;
  * @Version 1.0
  */
 public class VolatileDemo {
+
+	public static Integer sumConut = 0;
+
 	public static void main(String[] args) throws InterruptedException {
 		Sync sync = new VolatileDemo().new Sync();
 		sync.start();
@@ -17,9 +21,10 @@ public class VolatileDemo {
 
 	class Sync extends Thread {
 
+
 		// MESI 协议 Modify exclusive Shard invalid
 		// 涉及到 JMM 内存模型 ，缓存一致性协议
-		public volatile boolean flag = false;
+		public boolean flag = false;
 
 		public boolean isFlag() {
 			return flag;
@@ -33,21 +38,45 @@ public class VolatileDemo {
 		@Override
 		public void run() {
 			System.out.println("start ");
-			System.out.println("-----" + flag);
-			int sum = 0;
+			long sum = 0;
+			System.out.println("start " + System.currentTimeMillis());
+			int count = 0;
 			while (!flag) {
 				// MESI 协议 Modify exclusive Shard invalid
 				// 此处涉及到 io 操作 ，故案例不行 ，IO操作会 刷新总线 bus
 				//System.out.println(123);
+				//				System.out.println(111);
 
 				// 此处未涉及到 io 操作，只是 cpu 密集型，故案例行得通 ，volatile 案例
-				if(++sum /100 == 0 ){
-					sum = 0;
+				//				System.out.println("sum " + sum);
+				//				sum++;
+
+				// 和 Java 编译器有关，如果编译的时候是永真的，如果是静态变量，是要去刷新主存的,如果不是永真，那么不去主存。
+				if (sum++ == 1000000l) {
+//					sumConut ++ ;
+//					sum++;
+//					sum++;
+					System.out.println();
+//					sumConut ++ ;
+					while (!flag) {
+						System.out.println("sum ---------------" + sum + "count " + count);
+						count++;
+						////					}
+						////					sum = 0;
+					}
+
 				}
-
+				//			System.out.println(sum);
 			}
+			System.out.println("end " + System.currentTimeMillis());
+		}
 
-			System.out.println(sum);
+		@Test
+		public void testValue() {
+			Integer i1 = new Integer(1000000);
+			Integer i2 = i1;
+			i1 = new Integer(2000000);
+			System.out.println("i1 " + i1 + "-- i2 " + i2);
 		}
 	}
 }
