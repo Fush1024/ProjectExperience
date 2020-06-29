ea484ad682bb174cef56a5e425e6f17163e5c355
lidengjin <13488601486@163.com>
2020-06-29 14:20:37 +0800

    多线程实现加和操作 ，fork-join, 线程池-atomic-countdownlatch

---
+++ development/src/test/java/com/quickly/devploment/thread/sum/CurrentSum.java
@@ -0,0 +1,94 @@
+package com.quickly.devploment.thread.sum;
+
+import org.junit.Test;
+
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Author lidengjin
+ * @Date 2020/6/29 2:06 下午
+ * @Version 1.0
+ */
+public class CurrentSum {
+
+	public static final int N = 100;
+	static CountDownLatch countDownLatch = new CountDownLatch(N);
+
+	static AtomicInteger ai = new AtomicInteger(0);
+
+	/**
+	 * 使用线程池 以及 AtomicInteger 实现加一操作
+	 *
+	 * @param args
+	 * @throws InterruptedException
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		ExecutorService exec = Executors.newFixedThreadPool(10);
+		for (int i = 0; i <= N; i++) {
+			final int s = i;
+			exec.execute(() -> {
+				System.out.println(Thread.currentThread().getName() + ":" + ai.getAndAdd(s));
+				countDownLatch.countDown();
+			});
+		}
+		countDownLatch.await();
+		System.out.println(ai.get());
+		exec.shutdown();
+	}
+
+	@Test
+	public void testForkJoin() {
+		ForkJoinPool forkJoinPool = new ForkJoinPool();
+		CountTask task = new CountTask(1, 100);
+		// 执行一个任务
+		Future<Integer> result = forkJoinPool.submit(task);
+		try {
+			System.out.println(result.get());
+		} catch (ExecutionException | InterruptedException e) {
+			System.out.println(e);
+		}
+	}
+
+	static class CountTask extends RecursiveTask<Integer> {
+		// 阈值
+		private static final int THRESHOLD = 20;
+
+		private final int start;
+
+		private final int end;
+
+		public CountTask(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+
+		@Override
+		protected Integer compute() {
+			int sum = 0;
+			// 如果任务足够小就计算任务
+			boolean canCompute = (end - start) <= THRESHOLD;
+			if (canCompute) {
+				for (int i = start; i <= end; i++) {
+					sum += i;
+				}
+			} else {
+				// 如果任务大于阈值，就分裂成两个子任务计算
+				int middle = (start + end) / 2;
+				CountTask leftTask = new CountTask(start, middle);
+				CountTask rightTask = new CountTask(middle + 1, end);
+				// 执行子任务
+				leftTask.fork();
+				rightTask.fork();
+				// 等待子任务执行完，并得到其结果
+				int leftResult = leftTask.join();
+				int rightResult = rightTask.join();
+				// 合并子任务
+				sum = leftResult + rightResult;
+			}
+			return sum;
+		}
+
+	}
+
+}
