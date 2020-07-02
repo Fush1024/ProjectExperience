1ca4715d08bd714810d8d60efab982de3e645a0c
lidengjin <13488601486@163.com>
2020-07-02 10:49:52 +0800

    return 和 finally 执行顺序问题。return 返回的是当时属性的快照

---
+++ development/src/test/java/com/quickly/devploment/safe/ReturnAndFinally.java
@@ -0,0 +1,34 @@
+package com.quickly.devploment.safe;
+
+/**
+ * @Author lidengjin
+ * @Date 2020/7/2 10:44 上午
+ * @Version 1.0
+ */
+public class ReturnAndFinally {
+
+	public static void main(String[] args) {
+		System.out.println(testReturn());
+	}
+
+	private static int testReturn() {
+		int i = 10;
+		try {
+			int s = 10/0;
+			return returnMethod(i);
+		} catch (Exception e) {
+			i = 20;
+			System.out.println("catch " + i);
+		} finally {
+			i = 30;
+			System.out.println("finally " + i);
+		}
+		return i;
+	}
+
+	private static int returnMethod(int i) {
+		System.out.println("return --" + i);
+//		throw new RuntimeException();
+		return i;
+	}
+}
