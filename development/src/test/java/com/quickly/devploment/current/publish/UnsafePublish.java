4e6a6f9b4644fac382bd1839f4e39fc568337f7b
lidengjin <13488601486@163.com>
2020-07-06 16:39:23 +0800

    juc -- thread pool sample demo

---
+++ development/src/test/java/com/quickly/devploment/current/publish/UnsafePublish.java
@@ -0,0 +1,23 @@
+package com.quickly.devploment.current.publish;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+
+@Slf4j
+public class UnsafePublish {
+
+	private String[] states = {"a", "b", "c"};
+
+	public String[] getStates() {
+		return states;
+	}
+
+	public static void main(String[] args) {
+		UnsafePublish unsafePublish = new UnsafePublish();
+		log.info("{}", Arrays.toString(unsafePublish.getStates()));
+
+		unsafePublish.getStates()[0] = "d";
+		log.info("{}", Arrays.toString(unsafePublish.getStates()));
+	}
+}
