8642f59c0ec8b1c6065a2ebb10836d41c7d980d5
lidengjin <13488601486@163.com>
2020-07-06 16:37:52 +0800

    juc -- Singleton 创建方式

---
+++ development/src/test/java/com/quickly/devploment/current/singleton/SingletonExample1.java
@@ -0,0 +1,24 @@
+package com.quickly.devploment.current.singleton;
+
+/**
+ * 懒汉模式
+ * 单例实例在第一次使用时进行创建
+ */
+public class SingletonExample1 {
+
+	// 私有构造函数
+	private SingletonExample1() {
+
+	}
+
+	// 单例对象
+	private static SingletonExample1 instance = null;
+
+	// 静态的工厂方法
+	public static SingletonExample1 getInstance() {
+		if (instance == null) {
+			instance = new SingletonExample1();
+		}
+		return instance;
+	}
+}
---
+++ development/src/test/java/com/quickly/devploment/current/singleton/SingletonExample2.java
@@ -0,0 +1,21 @@
+package com.quickly.devploment.current.singleton;
+
+/**
+ * 饿汉模式
+ * 单例实例在类装载时进行创建
+ */
+public class SingletonExample2 {
+
+    // 私有构造函数
+    private SingletonExample2() {
+
+    }
+
+    // 单例对象
+    private static SingletonExample2 instance = new SingletonExample2();
+
+    // 静态的工厂方法
+    public static SingletonExample2 getInstance() {
+        return instance;
+    }
+}
