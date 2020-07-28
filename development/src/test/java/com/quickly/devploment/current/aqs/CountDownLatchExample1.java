08dcb4c2a8d38a71922f388bbc4a89b825d2b062
lidengjin <13488601486@163.com>
2020-07-28 15:02:39 +0800

    juc 大纲 -分类

--- development/src/test/java/com/quickly/devploment/current/aqs/CountDownLatchExample1.java
+++ development/src/test/java/com/quickly/devploment/current/aqs/CountDownLatchExample1.java
@@ -39,4 +39,46 @@ public class CountDownLatchExample1 {
 		log.info("{}", threadNum);
 		Thread.sleep(100);
 	}
+
+	/*
+	 1 juc  分类
+	 	juc-locks  锁框架 {
+	 		是 synchronized ，wait notify 的增强版
+	 		ReentrantLock
+	 		ReentrantReadWriteLock
+	 		LockSupport
+	 		AbstractQueuedSynchronizer AQS
+	 		Condition
+	 		StampedLock
+	 	}
+	 	juc-atomic 原子类框架 {
+	 		Unsafe
+	 		AtomicInteger
+	 		AtomicReference
+	 		Atomic 数组
+	 		AtomicXXXFieldUpdater
+	 		LongAdder 更强的原子类
+	 	}
+	 	juc-sync 同步器框架 {
+	 		CountDownLatch ，内部实现 AQS
+	 		CyclicBarrier 内部通过ReentrantLock 和 Condition
+	 		Semaphore 内部通过 AQS
+	 		Exchanger
+	 		Phaser
+	 	}
+	 	juc-collections 集合框架 {
+	 		ConcurrentHashMap
+	 		ConcurrentSkipListMap
+	 		CopyOnWriteArrayList
+	 		CopyOnWriteArraySet
+	 		ConcurrentLinkedQueue
+	 		DelayQueue 延时堵塞队列
+	 	}
+	 	juc-executors 执行器框架 {
+	 		线程池 ThreadPool
+	 		计划线程池 ScheduledThreadPoolExecutor
+	 		Future 模式
+	 		ForkJoin 框架
+	 	}
+	 */
 }
